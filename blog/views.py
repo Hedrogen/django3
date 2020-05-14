2ca7e2c513b9979e2984180db83d43453f900cea
Hedrogen <iliyas23256@gmail.com>
2020-05-15 00:12:17 +0300

    add comment model

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.mail import send_mail
 from  django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
 from .models import Post
-
 ''' Версия без класса
 
 def post_list(request):
@@ -44,11 +44,18 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
 
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
 
-    if request.method=='POST':
+    if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading " {} "'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments:{}'.format(post.title, post_url, cd['name'], cd['comments'])
+            # send_mail(subject, message, 'gitaglotus@gmail.com', [cd['to']])
+            send_mail(subject, message, 'admin@blog.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})

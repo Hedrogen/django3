7db7500b9ac2e58f2fe5e1eb982bd48bdfb11000
Hedrogen <iliyas23256@gmail.com>
2020-06-01 21:02:30 +0300

    sitemap

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,8 +1,15 @@
+from django.utils.safestring import mark_safe
 from django import template
 from . .models import Post
+import markdown
 
 
 register = template.Library()
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
 
 @register.simple_tag
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
+from .feeds import LatestPostsFeed
 from django.urls import path
 from . import views
 
+
 app_name = 'blog'
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -9,4 +12,5 @@ urlpatterns = [
     #  path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
